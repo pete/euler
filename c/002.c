bfe06854041964f463a46920a12856a2d297a629
Pete Elmore <pete@debu.gs>
2010-06-13 22:38:08 -0700

    Inadvertently checked in bad things, wiping before I github this mess.

---
+++ c/002.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long sum_even_fibs(long limit)
+{
+	long sum = 2, a = 2, b = 8, tmp;
+
+	while(b <= limit) {
+		sum += b;
+		tmp = a + 4 * b;
+		a = b;
+		b = tmp;
+	}
+	return sum;
+}
+
+long which_limit(int argc, char **argv)
+{
+	long limit;
+
+	if(!argc)
+		return 4000000;
+
+	limit = atol(argv[0]);
+	if(!limit)
+		return 4000000;
+	return limit;
+}
+
+int main(int argc, char **argv)
+{
+	long limit;
+	limit = which_limit(argc - 1, argv + 1);
+	printf("%ld\n", sum_even_fibs(limit));
+	return 0;
+}
