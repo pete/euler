8981e84435ac69ac7e1ff82660e9440f96d238fe
Pete Elmore <pete@debu.gs>
2010-11-18 01:23:48 -0800

    Erlang (0.18s) and Lua (0.001s) solutions for #3.  I have done the Erlang one wrong.

---
+++ erlang/p003.erl
@@ -0,0 +1,35 @@
+% Find the largest prime factor of 600851475143.
+
+-module(p003).
+-export([main/1, largest_factor/1]).
+
+% Like in the Pez solution, we special-case 2 and just check which odd numbers
+% divide N until N is 1.  When N is 1, we've hit the largest factor.
+
+% I can't help but feel that I am somehow doing this algorithm wrong; it seems
+% like I should not have written five different largest_factor(...)s, one with a
+% case and one with a guard.  I have written something that is hard to read.
+largest_factor(2) ->
+	2;
+largest_factor(N) ->
+	case N rem 2 of
+		0 -> largest_factor(N div 2);
+		1 -> largest_factor(N, 3)
+	end.
+largest_factor(1, F) ->
+	F;
+largest_factor(N, F) when N rem F =:= 0 ->
+	largest_factor(N div F, F);
+largest_factor(N, F) ->
+	largest_factor(N, F + 2).
+% I'M NEW TO ERLANG!
+% 
+% ...
+%
+% ...
+%
+% Good night, everybody.
+
+main(_) ->
+	io:write(largest_factor(600851475143)),
+	io:fwrite("\n").
---
+++ lua/003.lua
@@ -0,0 +1,26 @@
+-- Find the largest prime factor of 600851475143.
+
+function factor_out(n, f)
+	while n % f == 0 do
+		n = n / f
+	end
+	return n
+end
+
+function largest_factor(n)
+	local f = 3
+
+	n = factor_out(n, 2)
+	if n == 1 then
+		return n
+	end
+
+	f = 1
+	while n ~= 1 do
+		f = f + 2
+		n = factor_out(n, f)
+	end
+	return f
+end
+
+print(largest_factor(600851475143))
