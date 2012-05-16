ee914a4ea526e9f3223ac65360b07ed6090c52ac
Pete Elmore <pete@debu.gs>
2012-05-15 21:16:58 -0700

    Inferno shell solution for #19.

---
+++ infernosh/019.sh
@@ -0,0 +1,15 @@
+#!/dis/sh
+
+# How many Sundays fell on the first of the month during the twentieth century
+# (1 Jan 1901 to 31 Dec 2000)?
+
+# A direct port of my bash solution for the same problem.  It takes about as
+# long to run.
+
+load std expr
+
+for month in ${expr 1 12 seq} {
+	for year in ${expr 1901 2000 seq} {
+		cal $month $year | grep '^ 1 ' &
+	}
+} | wc -l
